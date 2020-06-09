83179d2ca061d4e3c32a964f152b81dcae89cfde
kimsinhyoung <sh_kim@blockchainglobal.co.kr>
2020-06-09 09:19:37 +0900

    기본접근권한 복구

--- config/settings.py
+++ config/settings.py
@@ -312,7 +312,8 @@ REST_USE_JWT = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
